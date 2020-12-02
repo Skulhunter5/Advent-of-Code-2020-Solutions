e4377682e2058f2b7964fa532111d00b8628865b
Skulhunter5 <62426864+Skulhunter5@users.noreply.github.com>
2020-12-02 17:27:42 +0100

    Added solution for day 2

---
+++ day 2/part_one.py
@@ -0,0 +1,33 @@
+with open("res/passwords", 'r') as file:
+	lines = file.readlines()
+'''
+Replace "res/passwords" with the path of your file containing the inputs or
+replace "file.readlines()" with a string containing the inputs
+'''
+
+numberAllPasswords = 0
+numberWrongPasswords = 0
+
+for line in lines:
+	numberAllPasswords += 1
+	temp = line.split(': ')
+	condition = temp[0]
+	password = temp[1]
+	temp = condition.split(' ')
+	letter = temp[1]
+	temp = temp[0].split('-')
+	minTimes = int(temp[0])
+	maxTimes = int(temp[1])
+
+	counter = 0
+	for l in password:
+		if(l == '\n'):
+			continue
+		if(l == letter):
+			counter += 1
+
+	if(counter < minTimes or counter > maxTimes):
+		numberWrongPasswords += 1
+
+print("Wrong passwords: " + str(numberWrongPasswords))
+print("Correct passwords: " + str(numberAllPasswords - numberWrongPasswords))
---
+++ day 2/part_two.py
@@ -0,0 +1,26 @@
+with open("res/passwords", 'r') as file:
+	lines = file.readlines()
+'''
+Replace "res/passwords" with the path of your file containing the inputs or
+replace "file.readlines()" with a string containing the inputs
+'''
+
+nTotalPasswords = 0
+nWrongPasswords = 0
+
+for line in lines:
+	nTotalPasswords += 1
+	temp = line.split(': ')
+	condition = temp[0]
+	password = temp[1]
+	temp = condition.split(' ')
+	letter = temp[1]
+	temp = temp[0].split('-')
+	pos1 = int(temp[0]) - 1
+	pos2 = int(temp[1]) - 1
+
+	if(not ((password[pos1] == letter) != (password[pos2] == letter))):
+		nWrongPasswords += 1
+
+print("Invalid passwords: " + str(nWrongPasswords))
+print("Valid passwords: " + str(nTotalPasswords - nWrongPasswords))
