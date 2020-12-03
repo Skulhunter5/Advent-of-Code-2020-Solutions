fecf7d247539a770d7bdae8e25a1fd12a9acc9fb
Skulhunter5 <62426864+Skulhunter5@users.noreply.github.com>
2020-12-03 07:01:38 +0100

    Added solution for day 3

--- day 3/part1.py
+++ day 3/part1.py
@@ -1,3 +1,17 @@
 # Replace the following code block with how you want to load your inputs
-with open("passwords", 'r') as file:
-	lines = file.readlines()
+with open("trees", 'r') as file:
+	rows = file.readlines()
+for i in range(len(rows)):
+	rows[i] = rows[i].replace('\n', '')
+
+nTreesEncountered = 0
+
+x = 0
+y = 0
+while(y < len(rows)):
+	if(rows[y][x % len(rows[0])] == '#'):
+		nTreesEncountered += 1
+	x += 3
+	y += 1
+
+print(nTreesEncountered)
---
+++ day 3/part2.py
@@ -0,0 +1,39 @@
+# Replace the following code block with how you want to load your inputs
+with open("trees", 'r') as file:
+	rows = file.readlines()
+for i in range(len(rows)):
+	rows[i] = rows[i].replace('\n', '')
+
+result = 1
+for i in range(5):
+	nTreesEncountered = 0
+
+	xInc = 0
+	yInc = 0
+	if(i == 0):
+		xInc = 1
+		yInc = 1
+	elif(i == 1):
+		xInc = 3
+		yInc = 1
+	elif(i == 2):
+		xInc = 5
+		yInc = 1
+	elif(i == 3):
+		xInc = 7
+		yInc = 1
+	elif(i == 4):
+		xInc = 1
+		yInc = 2
+
+	x = 0
+	y = 0
+	while(y < len(rows)):
+		if(rows[y][x % len(rows[0])] == '#'):
+			nTreesEncountered += 1
+		x += xInc
+		y += yInc
+
+	result *= nTreesEncountered
+
+print(result)
