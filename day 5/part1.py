8b338c8f303b0440b1b1a2bfd19c97347146b195
Skulhunter5 <62426864+Skulhunter5@users.noreply.github.com>
2020-12-05 10:33:18 +0100

    Added solution for day 5

---
+++ day 5/part1.py
@@ -0,0 +1,28 @@
+# Replace the following code block with how you want to load your inputs
+with open("boarding passes", 'r') as file:
+	lines = file.readlines()
+for i in range(len(lines)):
+	lines[i] = lines[i].replace('\n', '')
+
+maxSeatID = 0
+for line in lines:
+	minRow = 0
+	maxRow = 127
+	for i in range(7):
+		if(line[i] == 'B'):
+			minRow = minRow + (maxRow + 1 - minRow) // 2
+		elif(line[i] == 'F'):
+			maxRow = maxRow - (maxRow + 1 - minRow) // 2
+	minCol = 0
+	maxCol = 7
+	for i in range(3):
+		if(line[7 + i] == 'R'):
+			minCol = minCol + (maxCol + 1 - minCol) // 2
+		elif(line[7 + i] == 'L'):
+			maxCol = maxCol - (maxCol + 1 - minCol) // 2
+	seatID = minRow * 8 + minCol
+	print(minRow, ":", minCol, ":", seatID)
+	if(seatID > maxSeatID):
+		maxSeatID = seatID
+
+print(maxSeatID)
---
+++ day 5/part2.py
@@ -0,0 +1,34 @@
+# Replace the following code block with how you want to load your inputs
+with open("boarding passes", 'r') as file:
+	lines = file.readlines()
+for i in range(len(lines)):
+	lines[i] = lines[i].replace('\n', '')
+
+seatIDs = []
+for i in range(128):
+	for j in range(8):
+		seatIDs.append(i * 8 + j)
+
+for line in lines:
+	minRow = 0
+	maxRow = 127
+	for i in range(7):
+		if(line[i] == 'B'):
+			minRow = minRow + (maxRow + 1 - minRow) // 2
+		elif(line[i] == 'F'):
+			maxRow = maxRow - (maxRow + 1 - minRow) // 2
+	minCol = 0
+	maxCol = 7
+	for i in range(3):
+		if(line[7 + i] == 'R'):
+			minCol = minCol + (maxCol + 1 - minCol) // 2
+		elif(line[7 + i] == 'L'):
+			maxCol = maxCol - (maxCol + 1 - minCol) // 2
+	seatID = minRow * 8 + minCol
+	seatIDs.remove(seatID)
+
+for i in range(128):
+	for j in range(8):
+		seatID = i * 8 + j
+		if(not seatID - 1 in seatIDs and not seatID + 1 in seatIDs and seatID in seatIDs):
+			print(seatID)
